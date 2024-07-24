9e8efcc3e574d128d3120de921bf1375150691c1
afrid-md <afrid@airditsoftware.com>
2024-07-24 15:32:06 +0530

    removed microsoft authentication from the code. Uninstalled passport and passport-microsoft library

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const { body } = require("express-validator");
-const passport = require("passport");
 
 //file imports
 const {
@@ -39,18 +38,6 @@ router.post(
     body("password", "Password must not be empty").notEmpty(),
   ],
   login
-);
-
-// Microsoft OAuth routes
-router.get("/outlook", passport.authenticate("microsoft"));
-
-router.get(
-  "/outlook/callback",
-  passport.authenticate("microsoft", { failureRedirect: "/" }),
-  (req, res) => {
-    // Successful authentication, redirect to your desired page
-    res.redirect("/dashboard");
-  }
 );
 
 //aircraft auth routes
